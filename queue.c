09953afe4637e4193cf20df96a5575b82cd314e9
Eric Muxagata <ericgmconrado@gmail.com>
2014-09-16 17:17:31 -0400

    .

--- queue.c
+++ queue.c
@@ -20,7 +20,7 @@ typedef struct node {
 	int is_head;			// 1 if node is head, 0 otherwise
 	int is_tail;			// 1 if node is tail, 0 otherwise
 } node;
-	
+
 typedef struct queue {
 	node *head;
 	node *tail;
@@ -55,7 +55,7 @@ queue_t queue_new() {
  * Prepend a void* to a queue (both specifed as parameters).  Return
  * 0 (success) or -1 (failure).
  */
-int 
+int
 queue_prepend(queue_t queue, void* item) {
 
 	// create a new node to hold the item
@@ -86,7 +86,7 @@ queue_prepend(queue_t queue, void* item) {
 	(queue->head)->left_node = new_node;
 	(queue->head)->is_head = 0;
 	queue->head = new_node; 
-	
+
 	(queue->length)++;
 
 	return 0;
@@ -101,7 +101,7 @@ queue_append(queue_t queue, void* item) {
 
 	// create a new node to hold the item
 	node *new_node;
-	new_node = (node *)malloc(sizeof(node));
+	new_node = (node *) malloc(sizeof(node));
 
 	// If this is the first node, all we need to do is set the "dummy"
 	// node's data pointer:
@@ -139,8 +139,10 @@ queue_append(queue_t queue, void* item) {
  */
 int
 queue_dequeue(queue_t queue, void** item) {
-    	// first check if the queue is empty
-    	if ((queue->length) <= 0) {
+
+	node *new_head;
+	// first check if the queue is empty
+	if ((queue->length) <= 0) {
 		*item = NULL;
 		return -1;
 	}
@@ -176,7 +178,6 @@ queue_dequeue(queue_t queue, void** item) {
 	}
 
 	// Otherwise, we just set the node right of the head to be the new head
-	node *new_head;
 	new_head = queue->head->right_node;
 	new_head->left_node = NULL;
 	new_head->is_head = 0;
@@ -216,18 +217,19 @@ int
 queue_free (queue_t queue) {
 	// TODO: can I use queue_iterate here somehow?
 	// TODO: check implementation
-	
+
+	node *current_node;
+
 	if (queue->length < 0) {
 		return -1;
 	}
-	
+
 	if (queue->length == 0) {
 		free(queue->head);
 		free(queue);
 		return 0;
 	}
 
-	node *current_node; 
 	current_node = queue->head;
 	node *next_node;
 	while (!current_node->is_tail) {

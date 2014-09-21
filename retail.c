d9aa1d838c7455ba4a8bb9192f31f7e3a6136069
hwh33 <hwh33@cornell.edu>
2014-09-21 04:01:19 -0400

    Retail store application running now. Need to check implementation though.

--- retail.c
+++ retail.c
@@ -24,8 +24,6 @@
 #define M_CUSTOMERS 10
 #define BUFFER_SIZE 10
 
-int debug = 1;
-
 // Semaphores to guard against concurrency issues
 semaphore_t space_sem;
 semaphore_t phone_sem;
@@ -44,26 +42,22 @@ int in, out;
 // The "produce" function
 int unpack(int *arg) {
 	int new_serial_number;
-	int employee_ID = minithread_id(); // for debugging
-	
-	if (debug) printf("Employee attempting an unwrap. Employee ID = %i\n", employee_ID);
-	semaphore_P(space_sem);
-	if (debug) printf("Employee beginning unwrap. Employee ID = %i\n", employee_ID);
-	
-	// "unwrap" a phone by generating a new serial number
-	// and placing it in the phone buffer 
-	if (debug) printf("Trying to grab global mutex...\n");
-	semaphore_P(global_mutex);
-	if (debug) printf("Got it.\n");
-	new_serial_number = current_serial_number++;
-	phone_buffer[in++] = new_serial_number;
-	printf("Phone unwrapped. Serial number = %i\n", new_serial_number); //TODO: remove this line
-	if (in >= BUFFER_SIZE) in = 0;
-	semaphore_V(global_mutex);
 
-	if (debug) printf("Employee putting phone on shelf. Employee ID = %i\n", employee_ID);
-	semaphore_V(phone_sem);
-	if (debug) printf("Employee has placed phone on shelf. Employee ID = %i\n", employee_ID);
+	while(1) {
+	
+		semaphore_P(space_sem);
+	
+		// "unwrap" a phone by generating a new serial number
+		// and placing it in the phone buffer 
+		semaphore_P(global_mutex);
+		new_serial_number = current_serial_number++;
+		phone_buffer[in++] = new_serial_number;
+		if (in >= BUFFER_SIZE) in = 0;
+		semaphore_V(global_mutex);
+
+		semaphore_V(phone_sem);
+
+	}
 
 	return 0;
 }
@@ -71,23 +65,22 @@ int unpack(int *arg) {
 // The "consume" function
 int purchase(int *arg) {
 	int purchased_serial_number;
-	int customer_ID = minithread_id(); // for debugging
 
-	if (debug) printf("Customer attempting a purchase. Customer ID = %i\n", customer_ID);
-	semaphore_P(phone_sem);
-	if (debug) printf("Customer beginning a purchase. Customer ID = %i\n", customer_ID);
+	while(1) {
 
-	// "purchase" a phone by retrieving its serial number
-	// from the phone buffer and printing it to stdout
-	semaphore_P(global_mutex);
-	purchased_serial_number = phone_buffer[out++];
-	printf("Phone purchased. Serial number = %i\n", purchased_serial_number);
-	if (out >= BUFFER_SIZE) out = 0;
-	semaphore_V(global_mutex);
+		semaphore_P(phone_sem);
 
-	if (debug) printf("Customer taking phone from shelf. Customer ID = %i\n", customer_ID);
-	semaphore_V(space_sem);
-	if (debug) printf("Customer has taken phone from shelf. Customer ID = %i\n", customer_ID);
+		// "purchase" a phone by retrieving its serial number
+		// from the phone buffer and printing it to stdout
+		semaphore_P(global_mutex);
+		purchased_serial_number = phone_buffer[out++];
+		printf("Phone purchased. Serial number = %i\n", purchased_serial_number);
+		if (out >= BUFFER_SIZE) out = 0;
+		semaphore_V(global_mutex);
+
+		semaphore_V(space_sem);
+
+	}
 
 	return 0;
 }
@@ -97,13 +90,11 @@ int initialize_threads(int *arg) {
 
 	// start the employees working
 	for (i = 0; i < N_EMPLOYEES; i++) {
-		if (debug) printf("Putting employee %i to work.\n", i);
 		minithread_fork(unpack, NULL);
 	}
 
 	// start the customers purchasing
 	for (i = 0; i< M_CUSTOMERS; i++) {
-		if (debug) printf("Letting customer %i into the store.\n", i);
 		minithread_fork(purchase, NULL);
 	}
 
@@ -115,9 +106,9 @@ int main() {
 	phone_sem = semaphore_create();
 	global_mutex = semaphore_create();
 
-	semaphore_initialize(space_sem, BUFFER_SIZE-1);
-	semaphore_initialize(phone_sem, -1);
-	semaphore_initialize(global_mutex, 0);
+	semaphore_initialize(space_sem, BUFFER_SIZE);
+	semaphore_initialize(phone_sem, 0);
+	semaphore_initialize(global_mutex, 1);
 
 	current_serial_number = 0;
 	in = out = 0;

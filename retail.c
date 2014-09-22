174f81a84dde408296db5b714750006bff6e94d3
hwh33 <hwh33@cornell.edu>
2014-09-21 23:29:26 -0400

    Cosmetic changes to retail.c

--- retail.c
+++ retail.c
@@ -74,23 +74,17 @@ int unpack(int *arg) {
 int purchase(int *arg) {
 	int purchased_serial_number;
 
-	//while(1) {
+	semaphore_P(phone_sem);
 
-		semaphore_P(phone_sem);
+	// "purchase" a phone by retrieving its serial number
+	// from the phone buffer and printing it to stdout
+	semaphore_P(global_mutex);
+	purchased_serial_number = phone_buffer[out++];
+	printf("Phone purchased by %d. Serial number = %i\n", minithread_id(), purchased_serial_number);
+	if (out >= BUFFER_SIZE) out = 0;
+	semaphore_V(global_mutex);
 
-		// "purchase" a phone by retrieving its serial number
-		// from the phone buffer and printing it to stdout
-		semaphore_P(global_mutex);
-		purchased_serial_number = phone_buffer[out++];
-		printf("Phone purchased by %d. Serial number = %i\n", minithread_id(), purchased_serial_number);
-		if (out >= BUFFER_SIZE) out = 0;
-		semaphore_V(global_mutex);
-
-		semaphore_V(space_sem);
-
-	//	minithread_yield();
-
-	//}
+	semaphore_V(space_sem);
 
 	return 0;
 }

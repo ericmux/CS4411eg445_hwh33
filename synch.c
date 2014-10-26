4f22f18a8b228ca08ae47f3f817716a3bb22198c
hwh33 <hwh33@cornell.edu>
2014-10-25 20:14:11 -0400

    removed unnecessary destruction_lock in synch.c

--- synch.c
+++ synch.c
@@ -17,11 +17,8 @@
  * Semaphores.
  */
 typedef struct semaphore {
-	queue_t waiting_q;
-	// the destruction lock is used to prevent two threads from trying
-	// to destroy the same semaphore
-	tas_lock_t destruction_lock;
-	int count;
+    queue_t waiting_q;
+    int count;
 } semaphore;
 
 
@@ -31,10 +28,9 @@ typedef struct semaphore {
  */
 semaphore_t semaphore_create() {
     semaphore_t new_semaphore = (semaphore *)malloc(sizeof(semaphore));
-	new_semaphore->waiting_q = queue_new();
-	atomic_clear(&new_semaphore->destruction_lock);
+    new_semaphore->waiting_q = queue_new();
 	
-	return new_semaphore;
+    return new_semaphore;
 }
 
 /*
@@ -42,19 +38,15 @@ semaphore_t semaphore_create() {
  *      Deallocate a semaphore.
  */
 void semaphore_destroy(semaphore_t sem) {
-	// First thing we do is disable interrupts to prevent the OS from
-	// context switch while a thread is holding a semaphore lock. We
-	// will re-enable at the end of this function.
-	interrupt_level_t old_interrupt_level = set_interrupt_level(DISABLED);
+    // First thing we do is disable interrupts to prevent the OS from
+    // context switch while a thread is holding a semaphore lock. We
+    // will re-enable at the end of this function.
+    interrupt_level_t old_interrupt_level = set_interrupt_level(DISABLED);
 
-	while(atomic_test_and_set(&sem->destruction_lock)) {
-		set_interrupt_level(old_interrupt_level);
-		return;
-	}
-	queue_free(sem->waiting_q);
-	free(sem);
+    queue_free(sem->waiting_q);
+    free(sem);
 
-	set_interrupt_level(old_interrupt_level);
+    set_interrupt_level(old_interrupt_level);
 }
 
 
@@ -64,14 +56,14 @@ void semaphore_destroy(semaphore_t sem) {
  *      sem with an initial value cnt.
  */
 void semaphore_initialize(semaphore_t sem, int cnt) {
-	// First thing we do is disable interrupts to prevent the OS from
-	// context switch while a thread is holding a semaphore lock. We
-	// will re-enable at the end of this function.
-	interrupt_level_t old_interrupt_level = set_interrupt_level(DISABLED);
+    // First thing we do is disable interrupts to prevent the OS from
+    // context switch while a thread is holding a semaphore lock. We
+    // will re-enable at the end of this function.
+    interrupt_level_t old_interrupt_level = set_interrupt_level(DISABLED);
 
-	sem->count = cnt;
+    sem->count = cnt;
 
-	set_interrupt_level(old_interrupt_level);
+    set_interrupt_level(old_interrupt_level);
 }
 
 
@@ -81,20 +73,20 @@ void semaphore_initialize(semaphore_t sem, int cnt) {
  *      (a thread calling semaphore_P reflects a request for a resource)
  */
 void semaphore_P(semaphore_t sem) {
-	// First thing we do is disable interrupts to prevent the OS from
-	// context switch while a thread is holding a semaphore lock. We
-	// will re-enable at the end of this function.
-	interrupt_level_t old_interrupt_level = set_interrupt_level(DISABLED);	
+    // First thing we do is disable interrupts to prevent the OS from
+    // context switch while a thread is holding a semaphore lock. We
+    // will re-enable at the end of this function.
+    interrupt_level_t old_interrupt_level = set_interrupt_level(DISABLED);	
 
-	if (--sem->count < 0) {
-		// Resources are not available. Thread should be added to
-		// the waiting queue.
-		queue_append(sem->waiting_q, minithread_self());
-		set_interrupt_level(old_interrupt_level);
-		minithread_stop();
-	}
+    if (--sem->count < 0) {
+        // Resources are not available. Thread should be added to
+        // the waiting queue.
+        queue_append(sem->waiting_q, minithread_self());
+        set_interrupt_level(old_interrupt_level);
+        minithread_stop();
+    }
 
-	set_interrupt_level(old_interrupt_level);
+    set_interrupt_level(old_interrupt_level);
 }
 
 /*
@@ -103,21 +95,21 @@ void semaphore_P(semaphore_t sem) {
  *      (a thread calling semaphore_V reflects the yield of a resource)
  */
 void semaphore_V(semaphore_t sem) {
-	// First thing we do is disable interrupts to prevent the OS from
-	// context switch while a thread is holding a semaphore lock. We
-	// will re-enable at the end of this function.
-	// TODO: is this necessary?...
-	interrupt_level_t old_interrupt_level = set_interrupt_level(DISABLED);
+    // First thing we do is disable interrupts to prevent the OS from
+    // context switch while a thread is holding a semaphore lock. We
+    // will re-enable at the end of this function.
+    // TODO: is this necessary?...
+    interrupt_level_t old_interrupt_level = set_interrupt_level(DISABLED);
 
-	if (++sem->count <= 0) {
-		// Threads are waiting on resources, so pop one off the
-		// queue and start it
-		minithread_t thread_to_start;
-		queue_dequeue(sem->waiting_q, (void **)&thread_to_start);
-		set_interrupt_level(old_interrupt_level);
-		minithread_start(thread_to_start);
-		old_interrupt_level = set_interrupt_level(DISABLED);
-	}
+    if (++sem->count <= 0) {
+        // Threads are waiting on resources, so pop one off the
+        // queue and start it
+        minithread_t thread_to_start;
+        queue_dequeue(sem->waiting_q, (void **)&thread_to_start);
+        set_interrupt_level(old_interrupt_level);
+        minithread_start(thread_to_start);
+        old_interrupt_level = set_interrupt_level(DISABLED);
+    }
 
-	set_interrupt_level(old_interrupt_level);
+    set_interrupt_level(old_interrupt_level);
 }
